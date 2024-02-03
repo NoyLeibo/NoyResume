e0c8ede639db9fff9dc85f9bef14dbec1c9a9f89
NoyLeibo <150171476+NoyLeibo@users.noreply.github.com>
2024-02-03 16:52:40 +0200

    update about me page

--- src/cmps/AboutPage.jsx
+++ src/cmps/AboutPage.jsx
@@ -1,12 +1,10 @@
 import { TypeAnimation } from 'react-type-animation';
 import { MovingComponent } from 'react-moving-text';
 
-
 export function AboutPage() {
     const Letters = ['About me']
 
     const downloadFile = () => {
-        // Correct way to trigger a file download
         window.open("https://api.cloudinary.com/v1_1/dysh9q6ir/image/download?api_key=967872262144161&attachment=true&audit_context=eyJhY3Rvcl90eXBlIjoidXNlciIsImFjdG9yX2lkIjoiNGVjZjhhMWRmNmQzM2FkN2VhNmE1NTM2ZTU0MTdmNGEiLCJ1c2VyX2V4dGVybmFsX2lkIjoiYTM3OTRhYTRjN2MzNmYzOWM4NjU4MGM3NjcyOTIzIiwidXNlcl9jdXN0b21faWQiOiJub3lsZWlibzcwQGdtYWlsLmNvbSIsImNvbXBvbmVudCI6ImNvbnNvbGUifQ%3D%3D&public_id=Noy_Leibovich_-_Full_Stack_Developer_13_1_evspsu&signature=9adb997e70bf93ea023e8e3c8aa57faa4d94bcb2&source=ml&target_filename=Noy_Leibovich_-_Full_Stack_Developer_13_1_evspsu&timestamp=1706965668&type=upload", "_blank");
         console.log('Download initiated');
     }
@@ -14,7 +12,6 @@ export function AboutPage() {
 
     return (
         <section className="about-page flex row align-center">
-            {/* <img src="https://res.cloudinary.com/dysh9q6ir/image/upload/v1706788350/%D7%9C%D7%9C%D7%90_%D7%A9%D7%9D_ngeib7.png" className="about-me-img" /> */}
             <div className='fs22 about-details flex column'>
                 {Letters.map((letter) =>
                     <MovingComponent
@@ -28,22 +25,16 @@ export function AboutPage() {
                         fillMode="none">
                         {letter}
                     </MovingComponent>)}
-                {/* <TypeAnimation className='fs32 underline bold'
-                    sequence={["About me", 1000, "", 1, "About me", 1000]}
-                    speed={50}
-                    repeat={Infinity}
-                /> */}
-                My name is Noy Leibovich and I'm a
-                <TypeAnimation
-                    sequence={[" Programmer", 1000, " Developer", 1000]}
-                    speed={50}
-                    repeat={Infinity}
-                />
-                Seeking my frst role as a Full-Stack Developer, I bring a passion for coding, and
-                detail-oriented attitude. Eager to apply my love for technology in a dynamic and
-                innovative environment.
+                <p>I am Noy Leibovich, a full-stack {" "}
+                    <TypeAnimation
+                        className='underline'
+                        sequence={["porogrammer.", 2000, "developer.", 2000]}
+                        speed={50}
+                        repeat={Infinity}
+                    /></p>
+                <p>with proficiency in modern web technologies including JavaScript, React, Vue, Angular, Node.js, and Python. My coding journey is marked by a continuous quest for learning and applying the latest technologies to solve real-world problems.</p>
                 <button onClick={downloadFile} className='downloadcv bold fs16'>Download CV</button>
             </div>
-        </section>
+        </section >
     )
 }
