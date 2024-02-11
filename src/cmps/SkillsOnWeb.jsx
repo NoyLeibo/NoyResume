642e493006927a8bade0a4e87f106bdc6b63f36f
NoyLeibo <150171476+NoyLeibo@users.noreply.github.com>
2024-02-11 14:11:14 +0200

    change skills on web

--- src/cmps/SkillsOnWeb.jsx
+++ src/cmps/SkillsOnWeb.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import { ChartOne } from './ChartOne';
 import { ChartTwo } from './ChartTwo';
 
 export function SkillsOnWeb() {
+    const [clicked, setClicked] = useState(null);
 
+    const handleClick = (id) => {
+        setClicked(clicked === id ? null : id);
+        setTimeout(() => {
+            setClicked(null)
+        }, 400);
+        setClicked(clicked === id ? null : id);
+    };
     return (
-        <section className="skills-page flex row align-center justify-center">
-            <img src="./public/img/React-icon.svg.png" className='' />
-
-            {/* <ChartOne /> */}
-            {/* <ChartTwo /> */}
+        <section className='test'>
+            <section className="skills-page">
+                {['angular-icon.png', 'node-icon.png', 'React-icon.png', 'vue-icon.png', 'html5-css3-icon.png', 'js-icon.png'].map((icon, index) => (
+                    <img
+                        key={icon}
+                        src={`./public/img/${icon}`}
+                        className={`${clicked === index ? 'clicked' : ''}`}
+                        onClick={() => handleClick(index)}
+                    />
+                ))}
+            </section>
         </section>
+        // {/* <ChartOne /> */}
+        // {/* <ChartTwo /> */}
     );
 }
+
