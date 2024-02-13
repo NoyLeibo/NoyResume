85c9d4e5a7d0ee5da75567a115e5f50ff55cc763
NoyLeibo <150171476+NoyLeibo@users.noreply.github.com>
2024-02-13 18:11:25 +0200

    update paths and mobile

--- src/cmps/SkillsOnWeb.jsx
+++ src/cmps/SkillsOnWeb.jsx
@@ -1,11 +1,11 @@
 export function SkillsOnWeb() {
     const skills = [
-        { name: 'Angular', level: 100, icon: '/img/angular-icon.png' },
-        { name: 'Node.js', level: 100, icon: '/img/node-icon.png' },
-        { name: 'React', level: 100, icon: '/img/React-icon.png' },
-        { name: 'Vue', level: 100, icon: '/img/vue-icon.png' },
-        { name: 'HTML5 & CSS3', level: 100, icon: '/img/html5-css3-icon.png' },
         { name: 'JavaScript', level: 100, icon: '/img/js-icon.png' },
+        { name: 'HTML5 & CSS3', level: 100, icon: '/img/html5-css3-icon.png' },
+        { name: 'React', level: 100, icon: '/img/React-icon.png' },
+        { name: 'Angular', level: 100, icon: '/img/angular-icon.png' },
+        { name: 'Vue', level: 100, icon: '/img/vue-icon.png' },
+        { name: 'Node.js', level: 100, icon: '/img/node-icon.png' },
     ];
 
     return (
