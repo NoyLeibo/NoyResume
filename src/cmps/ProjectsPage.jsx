e4b1e5df58624baf1776b6f5487d7a9028e3e622
NoyLeibo <150171476+NoyLeibo@users.noreply.github.com>
2024-02-15 22:15:02 +0200

    Update projects cards

--- src/cmps/ProjectsPage.jsx
+++ src/cmps/ProjectsPage.jsx
@@ -3,13 +3,43 @@ export function ProjectsPage() {
         <section className="projects-page divider flex column align-center">
             <div className="fs32 underline">My-Projects</div>
             <div className="project-cards flex row">
-                <div className="project-card-one">
+
+                <div className="project-card-one flex column">
                     <img src="/img/AirBin.png" />
+                    <div className="project-text flex column ">
+                        <h1 className="underline bold">Air-Bin</h1>
+                        <div className="fs14"><span className="underline bold fs16">My final project.</span><br />an innovative marketplace app inspired by Airbnb, designed to connect guests with unique homes and experiences around the globe.<br />
+                            Using Javascript, React, Scss, MongoDB. <br /><span className="underline bold fs16">FULL-STACK APP</span>
+                        </div>
+                        <div className="bold">
+                            <span> - </span>
+                            <span onClick={() => window.open('https://github.com/NoyLeibo/AirBin')} className="underline pointer">Open Git</span>
+                            <span> - </span>
+                            <span onClick={() => window.open('https://airbin.onrender.com/')} className="underline pointer">Open App</span>
+                            <span> - </span>
+                        </div>
+                    </div>
                 </div>
-                <div className="project-card-two"></div>
-                <div className="project-card-three"></div>
-                <div className="project-card-four"></div>
+
+                <div className="project-card-two flex column">
+                    <img src="/img/Meme-Magic.png" />
+                    <div className="project-text flex column ">
+                        <h1 className="underline bold">Meme-Magic</h1>
+                        <div className="fs14">My second project in Javascript. <br /><span className="underline bold fs16">FROND-END APP</span>
+                        </div>
+                        <div className="bold">
+                            <span> - </span>
+                            <span onClick={() => window.open('https://github.com/NoyLeibo/Meme-Proj')} className="underline pointer">Open Git</span>
+                            <span> - </span>
+                            <span onClick={() => window.open('https://noyleibo.github.io/Meme-Proj/')} className="underline pointer">Open App</span>
+                            <span> - </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="project-card-three flex column"></div>
+                {/* <div className="project-card-four"></div> */}
             </div>
         </section>
     )
-}
+} 
